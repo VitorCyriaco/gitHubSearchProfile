6293c667da9aecf20248982c9f56bed25a328243
Vitor Cyriaco <vitorcyriaco@outlook.com>
2025-04-25 15:25:08 -0300

    feat: created background layout

---
+++ src/components/background.tsx
@@ -0,0 +1,15 @@
+export function Background(){
+    return(
+        <>
+        <div className="fixed top-[28px] left-[71px]">
+            <img className="w-[239px] h-[225px]" src="/src/assets/img/bg-points.png" alt="" />
+        </div>
+        <div className="fixed top-0 right-0">
+            <img src="/src/assets/img/bg-elipse.png" alt="" />
+        </div>
+        <div className="fixed top-[198px] left-0">
+            <img src="/src/assets/img/bg-elipse-left.png" alt="" />
+        </div>
+        </>
+    )
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,9 @@
+import { Background } from "../components/background";
+
+export default function Home(){
+    return(
+        <div className="w-full h-full">
+            <Background />
+        </div>
+    )
+}
